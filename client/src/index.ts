b4d11d08724abdb2b2e8d502aeda1115115f034c
zyoshoka <107108195+zyoshoka@users.noreply.github.com>
2024-09-06 23:42:54 +0900

    test: follow and unfollow at same time

--- client/src/index.ts
+++ client/src/index.ts
@@ -118,61 +118,44 @@ const [twoAdminInOneServer, oneAdminInTwoServer] = await Promise.all([
 	resolveAdmin('two.local', 'one.local', twoAdminClient),
 ]);
 
-if (
-	(await (oneAdminClient.request as Request)('users/following', { userId: oneAdmin.id }))
-		.some(v => v.followeeId === twoAdminInOneServer.object.id)
-) {
-	// remote assertion
+// follow @admin@one.local ==> @admin@two.local
+console.log(`Following @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local ...`);
+await (oneAdminClient.request as Request)('following/create', { userId: twoAdminInOneServer.object.id });
+console.log(`Followed @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local`);
+
+// wait for 1 secound
+await new Promise(resolve => setTimeout(resolve, 1000));
+
+await Promise.all([
+	assert.equal(
+		(await (oneAdminClient.request as Request)('users/following', { userId: oneAdmin.id }))
+			.some(v => v.followeeId === twoAdminInOneServer.object.id),
+		true,
+	),
 	assert.equal(
 		(await (twoAdminClient.request as Request)('users/followers', { userId: twoAdmin.id }))
 			.some(v => v.followerId === oneAdminInTwoServer.object.id),
 		true,
-	);
+	),
+]);
 
-	console.log(`Unfollowing @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local ...`);
-	await (oneAdminClient.request as Request)('following/delete', { userId: twoAdminInOneServer.object.id });
-	console.log(`Unfollowed @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local`);
+// unfollow @admin@one.local ==> @admin@two.local
+console.log(`Unfollowing @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local ...`);
+await (oneAdminClient.request as Request)('following/delete', { userId: twoAdminInOneServer.object.id });
+console.log(`Unfollowed @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local`);
 
-	// wait for 1 secound
-	await new Promise(resolve => setTimeout(resolve, 1000));
+// wait for 1 secound
+await new Promise(resolve => setTimeout(resolve, 1000));
 
-	await Promise.all([
-		assert.equal(
-			(await (oneAdminClient.request as Request)('users/following', { userId: oneAdmin.id }))
-				.some(v => v.followeeId === twoAdminInOneServer.object.id),
-			false,
-		),
-		assert.equal(
-			(await (twoAdminClient.request as Request)('users/followers', { userId: twoAdmin.id }))
-				.some(v => v.followerId === oneAdminInTwoServer.object.id),
-			false,
-		),
-	]);
-} else {
-	// remote assertion
+await Promise.all([
+	assert.equal(
+		(await (oneAdminClient.request as Request)('users/following', { userId: oneAdmin.id }))
+			.some(v => v.followeeId === twoAdminInOneServer.object.id),
+		false,
+	),
 	assert.equal(
 		(await (twoAdminClient.request as Request)('users/followers', { userId: twoAdmin.id }))
-			.some(v => v.followeeId === oneAdminInTwoServer.object.id),
+			.some(v => v.followerId === oneAdminInTwoServer.object.id),
 		false,
-	);
-
-	console.log(`Following @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local ...`);
-	await (oneAdminClient.request as Request)('following/create', { userId: twoAdminInOneServer.object.id });
-	console.log(`Followed @${ADMIN_PARAMS.username}@two.local from @${ADMIN_PARAMS.username}@one.local`);
-
-	// wait for 1 secound
-	await new Promise(resolve => setTimeout(resolve, 1000));
-
-	await Promise.all([
-		assert.equal(
-			(await (oneAdminClient.request as Request)('users/following', { userId: oneAdmin.id }))
-				.some(v => v.followeeId === twoAdminInOneServer.object.id),
-			true,
-		),
-		assert.equal(
-			(await (twoAdminClient.request as Request)('users/followers', { userId: twoAdmin.id }))
-				.some(v => v.followerId === oneAdminInTwoServer.object.id),
-			true,
-		),
-	]);
-}
+	),
+]);
